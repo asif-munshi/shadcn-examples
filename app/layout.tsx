c48e9bef05f1d095d516caefab923921a7ebf6a0
asif-munshi <asifmunshi2978@gmail.com>
2024-08-27 17:06:59 +0600

    replaced favicon, fonts, tailwind config and main-nav link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,42 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 
-import { Inter as FontSans } from "next/font/google";
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/layout/theme-provider";
 
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
 export const metadata: Metadata = {
-  title: "Shadcn Examples",
-  description: "Created by Asif Munshi",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: "shadcn",
+      url: "https://shadcn.com",
+    },
+    {
+      name: "Asif Munshi",
+      url: "https://github.com/asif-munshi/shadcn-examples",
+    },
+  ],
+  creator: "Asif Munshi",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "Shadcn Examples",
+  url: "https://ui.shadcn.com",
+  description: "shadcn/ui Examples. Open Source, Zero Analytics.",
+  links: {
+    github: "https://github.com/asif-munshi/shadcn-examples",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
